c3215b5c9d4d56a433e6eae1e486729f7a2d9693
0xk3v <kevin_@tuta.io>
2024-10-19 23:59:03 +0200

    added Prisma user interface inheritance

--- apps/backend/src/auth/dto/signup.dto.ts
+++ apps/backend/src/auth/dto/signup.dto.ts
@@ -1,6 +1,7 @@
+import { Prisma } from '@prisma/client';
 import { IsEmail, IsNotEmpty, IsString } from 'class-validator';
 
-export class SignupDto {
+export class SignupDto implements Prisma.UserCreateInput {
   @IsString()
   @IsNotEmpty()
   firstName: string;
