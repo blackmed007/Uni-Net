97f791747ee6caf699101ab90cb83945fb8cff68
0xk3v <kevin_@tuta.io>
2024-10-28 23:13:21 +0100

    added new methods to handle POST requests for Sign in and Sign up

--- apps/backend/src/auth/auth.controller.ts
+++ apps/backend/src/auth/auth.controller.ts
@@ -1,7 +1,19 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post, ValidationPipe } from '@nestjs/common';
 import { AuthService } from './auth.service';
+import { SignupDto } from './dto/signup.dto';
+import { SigninDto } from './dto/signin.dto';
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
+
+  @Post('/signup')
+  signUp(@Body(new ValidationPipe()) authDto: SignupDto) {
+    return this.authService.signUp(authDto);
+  }
+
+  @Post('/signin')
+  signIn(@Body(new ValidationPipe()) authDto: SigninDto) {
+    return this.authService.signIn(authDto);
+  }
 }
