7e840b68666f1b0d42bbc9157e91aa7590add4bd
0xk3v <kevin_@tuta.io>
2024-12-25 00:18:42 +0100

    added the first handler for POST event/

---
+++ apps/backend/src/event/event.service.ts
@@ -0,0 +1,78 @@
+import {
+  BadRequestException,
+  ForbiddenException,
+  HttpStatus,
+  Injectable,
+  InternalServerErrorException,
+  Logger,
+} from '@nestjs/common';
+import { CreateEventDto } from './dto/create-event.dto';
+import { UpdateEventDto } from './dto/update-event.dto';
+import { PrismaService } from 'src/prisma/prisma.service';
+import {
+  PrismaClientKnownRequestError,
+  PrismaClientValidationError,
+} from '@prisma/client/runtime/library';
+
+@Injectable()
+export class EventService {
+  constructor(private readonly prisma: PrismaService) {}
+
+  private readonly logger = new Logger(EventService.name, {
+    timestamp: true,
+  });
+
+  create(createEventDto: CreateEventDto) {
+    console.log(createEventDto);
+
+    try {
+      const { event_date, event_time, ...eventDto } = createEventDto;
+
+      const eventData = {
+        ...eventDto,
+        datetime: `${event_date}T${event_time}`,
+      };
+
+      return this.prisma.event.create({
+        data: eventData,
+      });
+    } catch (error) {
+      this.logger.error(`${error} - Error while creating event`);
+
+      if (error instanceof PrismaClientKnownRequestError) {
+        if (error.code === 'P2002') {
+          this.logger.error(
+            `${HttpStatus.FORBIDDEN} - Error while creating event`,
+          );
+          throw new ForbiddenException('Error while creating event');
+        }
+      } else if (error instanceof PrismaClientValidationError) {
+        this.logger.error(error.message);
+        throw new BadRequestException(
+          'Invalid date format. Expected ISO-8601 DateTime',
+        );
+      } else {
+        throw new InternalServerErrorException(
+          'Server error - please try again later',
+        );
+      }
+    }
+  }
+
+  findAll() {
+    return `This action returns all event`;
+  }
+
+  findOne(id: number) {
+    return `This action returns a #${id} event`;
+  }
+
+  update(id: number, updateEventDto: UpdateEventDto) {
+    console.log(updateEventDto);
+    return `This action updates a #${id} event`;
+  }
+
+  remove(id: number) {
+    return `This action removes a #${id} event`;
+  }
+}
