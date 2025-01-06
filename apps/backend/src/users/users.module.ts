e7154c85e0f50a547ae9651cb6136c419c799d05
0xk3v <kevin_@tuta.io>
2025-01-06 23:24:03 +0100

    imported 'image' service

--- apps/backend/src/users/users.module.ts
+++ apps/backend/src/users/users.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
+import { ImagesService } from 'src/images/images.service';
 
 @Module({
   controllers: [UsersController],
-  providers: [UsersService],
+  providers: [UsersService, ImagesService],
 })
 export class UsersModule {}
