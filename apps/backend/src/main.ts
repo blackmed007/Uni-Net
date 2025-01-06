59bf736a5716259f666ded0c7fa7dfa20f5d258d
0xk3v <kevin_@tuta.io>
2025-01-06 23:51:09 +0100

    swagger setup

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -12,6 +13,16 @@ async function bootstrap() {
 
   // Global prefix
   app.setGlobalPrefix('api/v1');
+
+  const options = new DocumentBuilder()
+    .addBearerAuth()
+    .setTitle('Nest-js Swagger Example API')
+    .setDescription('Swagger Example API API description')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, options);
+  // Swagger path: http://localhost:3200/api/docs
+  SwaggerModule.setup(`${'api/v1'}/docs`, app, document);
 
   app.useGlobalPipes(
     new ValidationPipe({
