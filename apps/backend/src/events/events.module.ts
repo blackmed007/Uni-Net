2b7b139e45709e483363cd0c47cc1cc7313477b1
0xk3v <kevin_@tuta.io>
2025-01-06 23:23:01 +0100

    imported image service to be used on the controller

--- apps/backend/src/events/events.module.ts
+++ apps/backend/src/events/events.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { EventService } from './events.service';
 import { EventController } from './events.controller';
+import { ImagesService } from 'src/images/images.service';
 
 @Module({
   controllers: [EventController],
-  providers: [EventService],
+  providers: [EventService, ImagesService],
 })
 export class EventModule {}
