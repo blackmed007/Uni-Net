024c811a48f2607f20f1a856ba243eadd10ca33c
0xk3v <kevin_@tuta.io>
2025-01-06 19:57:25 +0100

    added a new controller handler to be able to intecept an image and upload it

--- apps/backend/src/users/users.controller.ts
+++ apps/backend/src/users/users.controller.ts
@@ -9,9 +9,11 @@ import {
   UseGuards,
   Request,
   NotFoundException,
+  UseInterceptors,
+  UploadedFile,
 } from '@nestjs/common';
 import { UsersService } from './users.service';
-// import { UpdateUserDto } from './dto/update-user.dto';
+import { FileInterceptor } from '@nestjs/platform-express';
 import { OnboardUserDto } from './dto/onboard-user.dto';
 import { JwtGuard } from 'src/auth/guard';
 import { Request as ExpressRequest } from 'express';
@@ -22,14 +24,20 @@ export class UsersController {
 
   @Post('onboard')
   @UseGuards(JwtGuard)
-  onboard(
+  @UseInterceptors(FileInterceptor('profile_url')) // 'profile_url' is the name of the file input
+  async onboard(
     @Request() req: ExpressRequest,
+    @UploadedFile() file: Express.Multer.File,
     @Body() onboardUserDto: OnboardUserDto,
   ) {
     const userId = (req.user as { id: string }).id;
 
     if (!userId) {
       throw new NotFoundException('User not found');
+    }
+    if (file) {
+      const profileUrl = await this.usersService.uploadImage(file, userId);
+      onboardUserDto.profile_url = profileUrl; // Set the profile_url to the URL returned from uploadImage
     }
 
     return this.usersService.onboard(userId, onboardUserDto);
