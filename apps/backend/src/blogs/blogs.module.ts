99033d3436c5ab48ac81d6ef04f3dd7dc0b7a5ed
0xk3v <kevin_@tuta.io>
2025-01-26 15:56:25 +0100

    fixed blog error

--- apps/backend/src/blogs/blogs.module.ts
+++ apps/backend/src/blogs/blogs.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { BlogsService } from './blogs.service';
 import { BlogsController } from './blogs.controller';
+import { ImagesService } from 'src/images/images.service';
 
 @Module({
   controllers: [BlogsController],
-  providers: [BlogsService],
+  providers: [BlogsService, ImagesService],
 })
 export class BlogsModule {}
